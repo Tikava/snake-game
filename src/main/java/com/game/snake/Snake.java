6f0c97b743456f825c8e952f9a4bc3ab530cde0c
Tikava <wavoleniu@gmail.com>
2023-04-16 11:30:40 +0600

    main files of game

--- src/main/java/com/game/snake/Snake.java
+++ src/main/java/com/game/snake/Snake.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Snake extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Snake.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Game!");
         stage.setScene(scene);
         stage.show();
     }
